4bb9f63775dbc3ba141279fdfdbb9e894952021b
Guilherme Ruiz <57478888+guigoruiz1@users.noreply.github.com>
2023-02-04 00:02:55 +0000

    data cleanup and discord bot updates

--- source/discord_bot.py
+++ source/discord_bot.py
@@ -5,6 +5,8 @@ import subprocess
 import discord
 import asyncio
 import io
+import re
+import pandas as pd
 from enum import Enum
 import multiprocessing as mp
 import yugiquery as yq
@@ -35,47 +37,58 @@ def init_reports_enum():
 secrets = load_secrets('../assets/secrets.env')
 intents = discord.Intents(messages=True, guilds=True, members=True)
 bot = commands.Bot(command_prefix='/', intents=intents)
-checks = discord.app_commands.checks
 Reports = init_reports_enum()
 process = None
 
-def is_owner():
-    async def predicate(ctx):
-        return await bot.is_owner(ctx.user)
-    return checks.check(predicate)
-
-@bot.tree.command(name='shutdown', description='Shutdown bot')
-@is_owner()
+@bot.hybrid_command(name='shutdown', description='Shutdown bot', with_app_command = True)
+@commands.is_owner()
 async def shutdown(ctx):
-    await ctx.response.send_message(content='Shutting down...')
+    await ctx.send(content='Shutting down...')
     await bot.close()
 
-@bot.tree.command(name='run', description='Run full Yugiquery workflow')
-@is_owner()
-@checks.cooldown(1, 12*60*60, key=lambda i: (i.guild_id, i.user.id))
+@bot.hybrid_command(name='run', description='Run full Yugiquery workflow', with_app_command = True)
+@commands.is_owner()
+@commands.cooldown(1, 12*60*60, commands.BucketType.user)
 async def run(ctx, report: Reports):
     global process
     if process is not None:
-        await ctx.response.send_message(content='Query already running. Try again after it has finished.', ephemeral=True, delete_after=60)
+        await ctx.send(
+            content='Query already running. Try again after it has finished.', 
+            ephemeral=True,
+            delete_after=60
+        )
         return
     
-    await ctx.response.send_message(content='Initializing...', ephemeral=True, delete_after=60)
+    original_response = await ctx.send(
+        content='Initializing...', 
+        ephemeral=True, 
+        delete_after=60
+    )
     
     API_error = False
     def progress_handler(iterable=None, API_status=None, **kwargs):
         if iterable and ctx.channel.id != int(secrets['DISCORD_CHANNEL_ID']):
-                return discord_pbar(iterable, token = secrets['DISCORD_TOKEN'], channel_id=ctx.channel.id, file=io.StringIO(), **kwargs)
+                return discord_pbar(
+                    iterable, 
+                    token = secrets['DISCORD_TOKEN'], 
+                    channel_id=ctx.channel.id, 
+                    file=io.StringIO(),
+                    **kwargs
+                )
         elif API_status is not None:
             nonlocal API_error
             API_error = not API_status
             return
         
     try:
-        process = mp.Process(target=yq.run, args=[report.value, progress_handler])
+        process = mp.Process(
+            target=yq.run, 
+            args=[report.value, progress_handler]
+        )
         process.start()
-        await ctx.edit_original_response(content='Running...')
+        await original_response.edit(content='Running...')
     except:
-        await ctx.edit_original_response(content='Initialization failed!')
+        await original_response.edit(content='Initialization failed!')
     
     async def await_result():
         while process.is_alive():
@@ -97,48 +110,91 @@ async def run(ctx, report: Reports):
             await ctx.channel.send(content='Query execution aborted!')
         else:
             await ctx.channel.send(content=f'Query execution exited with exit code: {exitcode}')
+        
+    # Reset cooldown in case query did not complete
+    if API_error or exitcode!=1:
+        await ctx.command.reset_cooldown(ctx)
     
         
-@bot.tree.command(name='abort', description='Abort running Yugiquery workflow')
-@is_owner()
+@bot.hybrid_command(name='abort', description='Abort running Yugiquery workflow', with_app_command=True)
+@commands.is_owner()
 async def abort(ctx):
-
-    await ctx.response.send_message('Aborting...', ephemeral=True, delete_after=60)
+    original_response = await ctx.send(
+        content='Aborting...', 
+        ephemeral=True, 
+        delete_after=60
+    )
     
     try:
         process.terminate()
-        await ctx.edit_original_response(content='Aborted')
+        await original_response.edit(content='Aborted')
     except:
-        await ctx.edit_original_response(content='Abort failed')
-        
-@bot.tree.command(name='latest', description='Show latest time each report was generated')
+        await original_response.edit(content='Abort failed')
+
+@bot.hybrid_command(name='latest', description='Show latest time each report was generated', with_app_command=True)
 async def latest(ctx):
-    await ctx.response.defer()
+    await ctx.defer()
     response='Latest reports generated:'
     reports = sorted(glob.glob('../*.html'))
     for report in reports:
-        response += f'\n- {os.path.basename(report)[:-5]}: {yq.pd.to_datetime(os.path.getmtime(report),unit="s", utc=True).strftime("%d/%m/%Y %H:%M %Z")}'
+        response += f'\n- {os.path.basename(report)[:-5]}: {pd.to_datetime(os.path.getmtime(report),unit="s", utc=True).strftime("%d/%m/%Y %H:%M %Z")}'
     
-    await ctx.followup.send(response)
+    await ctx.send(content=response)
     
-@bot.tree.command(name='links', description='Show Yugiquery links')
+@bot.hybrid_command(name='links', description='Show Yugiquery links', with_app_command=True)
 async def links(ctx):
-    response = '\n'.join(
-        ['Webpage: https://guigoruiz1.github.io/yugiquery/',
-         'Repository: https://github.com/guigoruiz1/yugiquery/',
-         'Data: https://github.com/guigoruiz1/yugiquery/tree/main/data']
-    )
-    await ctx.response.send_message(response)
     
-@bot.tree.command(name='data', description='Send latest data files')
+    embed = discord.Embed(
+        title="Yugiquery links",
+        description='• [Webpage](https://guigoruiz1.github.io/yugiquery/)\n'\
+                    '• [Repository](https://github.com/guigoruiz1/yugiquery/)\n'\
+                    '• [Data](https://github.com/guigoruiz1/yugiquery/tree/main/data)',
+        color=discord.Colour.blue()
+    )
+    
+    await ctx.send(embed=embed)
+    
+@bot.hybrid_command(name='data', description='Send latest data files', with_app_command=True)
+@commands.cooldown(1, 24*60*60, commands.BucketType.user)
 async def data(ctx):
-    response = 'Under construction'
-    await ctx.response.send_message(response)
+    await ctx.defer()
+    
+    await ctx.send('Under construction')
+    
+#     original_response = await ctx.send(content='Sending requested files...')
+    
+#     reports = glob.glob('../data/*cards*.csv')
+#     files=pd.DataFrame({'File':reports})
+#     files['Timestamp'] = files['File'].apply(os.path.getctime)
+#     files['Group'] = files['File'].apply(lambda x: re.search(r'(\w+_\w+)_(\d+-\d+-\d+T\d+:\d+.*).csv', os.path.basename(x)).group(1))
+#     index = files.groupby('Group')['Timestamp'].idxmax()
+#     latest_files = files.loc[index,'File'].tolist()
+    
+#     files=[]
+#     for file_path in latest_files:
+#         with open(file_path, 'rb') as f:
+#             files.append(discord.File(f))
+    
+#     await original_response.edit(content='Latest files:', attachments=files)
 
-@bot.tree.command(name='ping', description='Test the bot connection latency')
+@bot.hybrid_command(name='ping', description='Test the bot connection latency', with_app_command=True)
 async def ping(ctx):
-    await ctx.response.send_message('Pong! {0}ms'.format(round(bot.latency*1000, 1)), ephemeral=True, delete_after=60)
+    await ctx.send(
+        content='Pong! {0}ms'.format(round(bot.latency*1000, 1)), 
+        ephemeral=True, 
+        delete_after=60
+    )
 
+@bot.hybrid_command(name='test', description='test', with_app_command=True)
+@commands.cooldown(1, 60, commands.BucketType.user)
+@commands.is_owner()
+async def test(ctx):
+    await ctx.send(
+        content=f'Teste efetuado, {ctx.author.name}', 
+        ephemeral=True, 
+        delete_after=60
+    )
+    
 @bot.event
 async def on_ready():
     print('You are logged as {0.user}'.format(bot))
@@ -157,13 +213,16 @@ async def on_message(message):
      
     await bot.process_commands(message)
     if message.content.lower().startswith('hi'):
-        await message.channel.send(f'Hello, {message.author.name}!')
-    
-@bot.tree.error
-async def on_error(ctx, error):
-    if isinstance(error, discord.app_commands.errors.CommandOnCooldown):
-        await ctx.response.send_message(error)
-    elif isinstance(error, discord.app_commands.errors.CheckFailure):
-        await ctx.response.send_message(error)
+        await message.channel.send(content=f'Hello, {message.author.name}!')
+
+@bot.event
+async def on_command_error(ctx, error):
+    print(error)
+    if isinstance(error, commands.errors.CommandOnCooldown):
+        await ctx.send(content=error)
+    elif isinstance(error, commands.errors.NotOwner):
+        await ctx.send(content=error)
+    elif isinstance(error, commands.errors.CheckFailure):
+        await ctx.send(content=error)
 
 bot.run(secrets['DISCORD_TOKEN'])
