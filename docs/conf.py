efc5baff55f6f9bbe41768b50c341a1e57c50438
Guilherme Ruiz <57478888+guigoruiz1@users.noreply.github.com>
2023-02-26 16:56:06 +0000

    Template index

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@
 
 import sys, os
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../source'))
 
 # -- Project information
 
---
+++ docs/index.rst
@@ -0,0 +1,4 @@
+My Project
+==========
+
+Welcome to the documentation for Yugiquery. This document provides an overview of the project and how to use it.
