d288e05980cc78315cb4b31450ce1dcaff85eefc
Guilherme Ruiz <57478888+guigoruiz1@users.noreply.github.com>
2023-12-14 11:22:11 +0000

    Update install.sh
    Better install script
    
    Signed-off-by: Guilherme Ruiz <57478888+guigoruiz1@users.noreply.github.com>

--- source/install.sh
+++ source/install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-CURRENT_DIR=$PWD
-cd "$(dirname "$0")"
+
+pushd "$(dirname "$0")"
 
 # Install Python packages
 pip3 install -U pip
@@ -12,38 +12,26 @@ pip3 install -U nbstripout
 
 # Install nbconvert template
 
-# Get the config directories from jupyter --paths output
-config_directories=$(jupyter --paths | awk '/data:/,/runtime:/ {if (!/data:/ && !/runtime:/) print}')
+# Get the Jupyter installation prefix
+jupyter_install_prefix=$(which jupyter | xargs dirname | xargs dirname)
 
-# Iterate over each config directory and copy the folder to nbconvert templates
-for config_dir in $config_directories; do
-    templates_directory="$config_dir/nbconvert/templates"
+# Set the destination templates directory
+templates_directory="$jupyter_install_prefix/share/jupyter/nbconvert/templates"
 
-    # Check if the nbconvert templates directory exists
-    if [ -d "$templates_directory" ]; then
-        # Create the destination folder if it does not exist
-        mkdir -p "$templates_directory/yugiquery"
-        
-        # Copy the folder to nbconvert templates directory
-        cp -r "../assets/nbconvert"/* "$templates_directory/yugiquery"
-        
-        # Check if the copy was successful
-        if [ $? -eq 0 ]; then
-            echo "nbconvert template successfully installed in $templates_directory"
-        else
-            echo "Error: Failed to install nbconvert template."
-            echo "Be sure to install it manually or change the template used when generating the HTML report."
-        fi
+# Create the destination folder if it does not exist
+mkdir -p "$templates_directory/yugiquery"
 
-        break  # Exit the loop if found
-    fi
-done
+# Copy the folder to nbconvert templates directory
+cp -r "../assets/nbconvert"/* "$templates_directory/yugiquery"
 
-# Check if nbconvert templates directory was not found
-if [ ! -d "$templates_directory" ]; then
-    echo "Error: Nbconvert templates directory not found in any Jupyter data directory."
-    echo "Be sure to install you have nbconvert installed and try again or install the template manualy."
+# Check if the copy was successful
+if [ $? -eq 0 ]; then
+    echo "yugiquery nbconvert template successfully installed in $templates_directory"
+else
+    echo "Error: Failed to install yugiquery nbconvert template."
+    echo "Be sure to install it manually or change the template used when generating the HTML report."
 fi
+
 # Finish
 
-cd $CURRENT_DIR
+popd
