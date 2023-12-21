a82fd230d1ba9919e3754ef41b1c1454df3bc7e8
Guilherme Ruiz <57478888+guigoruiz1@users.noreply.github.com>
2023-12-21 21:53:54 +0000

    Nbconvert (#24)
    * Minor edits
    
    * timeline report benchmarked - 2023-12-06T22:26:47.277343+00:00
    
    * Timeline update - 2023-12-06T22:22:50.632534+00:00
    
    * index and readme timestamp update - 2023-12-06T22:27:03.644268+00:00
    
    * rush report benchmarked - 2023-12-08T20:51:23.018700+00:00
    
    * Docstrings and black formatting
    
    * black formatting
    
    * Minor bug fix
    
    * bug fix attempt
    
    * correct bug fix
    
    * More documentation bug fix
    
    * bandai report benchmarked - 2023-12-11T05:02:26.605007+00:00
    
    * Bandai update - 2023-12-11T05:02:15.417164+00:00
    
    * cards report benchmarked - 2023-12-11T05:05:43.595833+00:00
    
    * Cards update - 2023-12-11T05:02:54.334269+00:00
    
    * rush report benchmarked - 2023-12-11T05:06:41.222194+00:00
    
    * Rush duel update - 2023-12-11T05:05:56.436722+00:00
    
    * sets report benchmarked - 2023-12-11T05:18:11.845038+00:00
    
    * Sets update - 2023-12-11T05:06:51.869986+00:00
    
    * speed report benchmarked - 2023-12-11T18:50:23.431179+00:00
    
    * Speed update - 2023-12-11T18:49:55.928175+00:00
    
    * index and readme timestamp update - 2023-12-11T18:50:34.013884+00:00
    
    * emergency bug fix
    
    * 12122023
    
    * nbconvert template and restructuring assets
    
    * Update install.sh
    
    Better install script
    
    Signed-off-by: Guilherme Ruiz <57478888+guigoruiz1@users.noreply.github.com>
    
    * better handling of requirements
    
    * changed nbconvert template name to labdynamic
    
    * changed default data file name from "all_report" to "report_data" - needs testing
    
    * better readme and directory structure
    
    * Excluding toc from section numbering
    
    * Jekyll update
    
    * better install script
    
    ---------
    
    Signed-off-by: Guilherme Ruiz <57478888+guigoruiz1@users.noreply.github.com>

--- source/install.sh
+++ source/install.sh
@@ -1,10 +1,48 @@
 #! /bin/bash
 CURRENT_DIR=$PWD
 cd "$(dirname "$0")"
+
+# Install Python packages
 pip3 install -U pip
 pip3 install -r requirements.txt
 pip3 install git+https://github.com/guigoruiz1/halo.git
 pip3 install git+https://github.com/guigoruiz1/tqdm.git
 pip3 install -U pynacl
 pip3 install -U nbstripout
+
+# Install nbconvert template
+
+# Get the second line after "data:" from jupyter --paths output
+config_directories=$(jupyter --paths | awk '/data:/ {getline; getline; print}')
+
+# Check if a valid config directory is found
+if [ -n "$config_directories" ]; then
+    templates_directory="$config_directories/nbconvert/templates"
+
+    # Check if the nbconvert templates directory exists
+    if [ -d "$templates_directory" ]; then
+        # Create the destination folder if it does not exist
+        mkdir -p "$templates_directory/yugiquery"
+        
+        # Copy the folder to nbconvert templates directory
+        cp -r "../assets/nbconvert"/* "$templates_directory/yugiquery"
+        
+        # Check if the copy was successful
+        if [ $? -eq 0 ]; then
+            echo "nbconvert template successfully installed in $templates_directory"
+        else
+            echo "Error: Failed to install nbconvert template."
+            echo "Be sure to install it manually or change the template used when generating the HTML report."
+        fi
+
+    else
+        echo "Error: Nbconvert templates directory not found in the specified Jupyter data directory."
+        echo "Be sure to install nbconvert and try again or install the template manually."
+    fi
+else
+    echo "Error: Data directory not found in the Jupyter paths output."
+    echo "Make sure Jupyter is installed and configured correctly."
+fi
+# Finish
+
 cd $CURRENT_DIR
