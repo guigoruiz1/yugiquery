bff6008ab76f032fda87dc3cfa39a083fb526b00
Guilherme Ruiz <57478888+guigoruiz1@users.noreply.github.com>
2024-09-08 20:45:18 +0100

    timeline report benchmarked - 2024-09-08T19:45:18.676600+00:00

--- yugiquery/yugiquery.py
+++ yugiquery/yugiquery.py
@@ -1771,7 +1771,7 @@ def run_notebooks(
                 kernel_name=kernel_name,
             )
         except pm.PapermillExecutionError as e:
-            print(e)
+            tqdm.write(e)
             exceptions.append(e)
         finally:
             os.environ.pop("PM_IN_EXECUTION", default=None)
