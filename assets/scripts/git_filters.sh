a8b67bd447c687b2a4828277fcf1bff00e6fde53
Guilherme Ruiz <57478888+guigoruiz1@users.noreply.github.com>
2024-09-13 23:28:45 +0100

    bug fix in filter installation

--- assets/scripts/git_filters.sh
+++ assets/scripts/git_filters.sh
@@ -4,9 +4,10 @@
 git config filter.strip_secrets.clean "sed 's/=[^=]*/=REDACTED/'"
 git config filter.strip_secrets.smudge "cat"
 
-# Check if the filter definition is already in the .gitattributes file, add it if not
-if ! grep -Fxq "**/*secret* filter=strip_secrets" .gitattributes; then
+# Check if the .gitattributes file exists and if it contains the filter definition
+if [ ! -f .gitattributes ] || ! grep -Fxq "**/*secret* filter=strip_secrets" .gitattributes; then
     echo "**/*secret* filter=strip_secrets" >> .gitattributes
+fi
 
 # Install nbstripout with the specified attributes file in the notebooks directory
 pip install nbstripout
