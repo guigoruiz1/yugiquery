dd748c80882c7b7417c0191edc6408c7d67e881e
Guilherme Ruiz <57478888+guigoruiz1@users.noreply.github.com>
2026-02-08 13:02:05 +0000

    filter metadata

--- assets/scripts/git_filters.sh
+++ assets/scripts/git_filters.sh
@@ -12,3 +12,4 @@ fi
 # Install nbstripout with the specified attributes file in the notebooks directory
 pip install nbstripout
 nbstripout --install --attributes "notebooks/.gitattributes"
+git config filter.nbstripout.extrakeys 'metadata.celltoolbar metadata.kernelspec metadata.language_info.codemirror_mode.version metadata.language_info.pygments_lexer metadata.language_info.version metadata.toc metadata.notify_time metadata.varInspector cell.metadata.heading_collapsed cell.metadata.hidden cell.metadata.code_folding cell.metadata.tags cell.metadata.init_cell'
